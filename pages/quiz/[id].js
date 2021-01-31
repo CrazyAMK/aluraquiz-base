16d1e510890a3d5a1f4d41ab2af4c733001d0305
Eduardo Amroginski Santos <eduardo.amroginski@pampa.compasso>
2021-01-31 01:59:17 -0300

    Alterando import

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import QuizScreen from '../../src/screens/Quiz'
+import { Quiz } from '../../src/screens/Quiz'
 
 export default function QuizDaGaleraPage({ dbExterno }){
     return (
         <div>
 
             <ThemeProvider theme={dbExterno.theme}>
-                <QuizScreen externalDb={dbExterno} />
+                <Quiz externalDb={dbExterno} />
             </ThemeProvider>
             {/* <pre style={{color: 'black'}}>
                 {JSON.stringify(dbExterno.questions, null, 4)}
